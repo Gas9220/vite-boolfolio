0288e5c39303fe8e798e8ada4a653ad2acb53e24
Gaspare <Gas9220@users.noreply.github.com>
2023-05-25 16:49:58 +0200

    Bootstrap icons import - footer styling

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,13 +5,31 @@ export default {
 </script>
 
 <template>
-    <footer>
-        <div>Footer</div>
+    <footer class="d-flex justify-content-end p-3">
+        <div class="social-links d-flex gap-2 me-3">
+            <a href="#" class="social-link fs-4"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-linkedin"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-telegram"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-github"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-google"></i></a>
+            <a href="#" class="social-link fs-4"><i class="bi bi-skype"></i></a>
+        </div>
     </footer>
 </template>
  
-<style scoped>
+<style scoped lang="scss">
 footer {
     background-color: #146C94;
+
+    .social-links {
+        .social-link {
+            color: #F6F1F1;
+
+            &:hover {
+                color: #AFD3E2;
+            }
+        }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import * as bootstrap from 'bootstrap';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './assets/scss/main.scss';
 import App from './App.vue';
 
