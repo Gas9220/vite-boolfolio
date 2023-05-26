bd0b995bf9e94cea750ff02071cc1d90aee87f26
Gaspare <Gas9220@users.noreply.github.com>
2023-05-26 14:56:31 +0200

    main.js router import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
+import { router } from './router';
+
 import * as bootstrap from 'bootstrap';
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 import './assets/scss/main.scss';
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
