97c4571df54952d58433e053c7e43fd7f60a20da
Gaspare <Gas9220@users.noreply.github.com>
2023-05-26 15:27:20 +0200

    ProjectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100 p-1">
+        <img v-if="project.project_image" :src="project.project_image" class="card-img-top rounded-5 mb-2"
+            :alt="project.project_name + '-img'">
+        <span v-if="project.type" class="badge tag me-2">{{ project.type.name + ' Project' }}</span>
+        <h5 class="card-title text-center">
+            {{ project.project_name }}
+        </h5>
+        <div class="card-body">
+            <p>{{ project.project_summary }}</p>
+            <span class="d-block">{{ 'Start date: ' + project.start_date }}</span>
+            <span>{{ 'End date: ' + project.end_date }}</span>
+            <h6>{{ 'Revenues: ' + project.revenues }}€</h6>
+            <div class="d-flex flex-wrap">
+                <div v-for="technology in project.technologies">
+                    <span class="badge tag me-2">{{ technology.name }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
     name: 'ProjectsPage',
+    components: {
+        ProjectCard
+    },
     data() {
         return {
             apiBaseUrl: 'http://127.0.0.1:8000/api',
@@ -36,25 +40,7 @@ export default {
         <div class="container">
             <div class="row gy-4 pt-4 pb-4">
                 <div v-for="project in projects" class="col-3">
-                    <div class="card h-100 p-1">
-                        <img v-if="project.project_image" :src="project.project_image" class="card-img-top rounded-5 mb-2"
-                            :alt="project.project_name + '-img'">
-                        <span v-if="project.type" class="badge tag me-2">{{ project.type.name + ' Project' }}</span>
-                        <h5 class="card-title text-center">
-                            {{ project.project_name }}
-                        </h5>
-                        <div class="card-body">
-                            <p>{{ project.project_summary }}</p>
-                            <span class="d-block">{{ 'Start date: ' + project.start_date }}</span>
-                            <span>{{ 'End date: ' + project.end_date }}</span>
-                            <h6>{{ 'Revenues: ' + project.revenues }}€</h6>
-                            <div class="d-flex flex-wrap">
-                                <div v-for="technology in project.technologies">
-                                    <span class="badge tag me-2">{{ technology.name }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ProjectCard :project="project"></ProjectCard>
                 </div>
             </div>
         </div>
