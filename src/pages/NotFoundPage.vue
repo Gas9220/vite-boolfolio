5f7dd28b297fc7a1f8dcb4786805ef6357137c34
Gaspare <Gas9220@users.noreply.github.com>
2023-05-29 15:40:47 +0200

    NotFoundPage UI

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "NotFoundPage"
+}
+</script>
+
+
+<template>
+    <section>
+        <h1 class="mt-5 mb-3">
+            This page doesn't exist
+        </h1>
+    </section>
+</template>
