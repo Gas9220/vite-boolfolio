cfdc227433f38b8727a431fa6635df473147a605
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 16:50:24 +0200

    Store technologiesListUrl

--- src/store.js
+++ src/store.js
@@ -4,7 +4,8 @@ const store = reactive({
     apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
     urlProjectsList: import.meta.env.VITE_API_PROJECTS_LIST_URL,
     projectDetailUrl: import.meta.env.VITE_API_PROJECT_DETAIL_URL,
-    typesListUrl: import.meta.env.VITE_API_TYPES_LIST_URL
+    typesListUrl: import.meta.env.VITE_API_TYPES_LIST_URL,
+    technologiesListUrl: import.meta.env.VITE_API_TECHNOLOGIES_LIST_URL 
 });
 
 export default store;
