6a0e1f715c225fcc53e3e2bc59f67d4b8e19fa57
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 18:46:38 +0200

    ContactsPage completed

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,27 +1,74 @@
 <script>
+import axios from 'axios';
+import store from '../store';
 
 export default {
-    name: 'TypesPage',
 
+    name: 'ContactsPage',
+    data() {
+        return {
+            store,
+            messageData: {
+                user: '',
+                message: '',
+                email: ''
+            },
+            resultMessage: ''
+        }
+    },
+    methods: {
+        sendMessage() {
+
+            const data = {
+                user: this.messageData.user,
+                message: this.messageData.message,
+                email: this.messageData.email
+            };
+
+            axios.post(this.store.messagesUrl, data)
+                .then((response) => {
+                    console.log(response)
+
+                    if (response.status === 201) {
+                        this.resultMessage = 'I received your message'
+                        this.clearFields()
+                    }
+
+                })
+                .catch((error) => {
+                    console.log(error)
+                    this.resultMessage = 'Failed to send message'
+                });
+
+        },
+        clearFields() {
+            this.messageData.user = ''
+            this.messageData.message = ''
+            this.messageData.email = ''
+        }
+    }
 }
 </script>
 
 <template>
     <main>
         <h1 class="text-center pt-2">Contact me</h1>
+        <h3 class="text-center text-success">{{ resultMessage }}</h3>
         <div class="container-fluid justify-content-center d-flex">
-            <form class="w-50 pb-3 pt-3">
+            <form @submit.prevent="sendMessage" class="w-50 pb-3 pt-3">
                 <div class="mb-3">
                     <label for="user" class="form-label">Your name</label>
-                    <input type="text" class="form-control" id="user" aria-describedby="user" name="user">
+                    <input type="text" class="form-control" id="user" aria-describedby="user" name="user"
+                        v-model="messageData.user" required>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
+                    <input type="email" class="form-control" id="email" aria-describedby="email" name="email"
+                        v-model="messageData.email" required>
                 </div>
                 <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="message"
-                        style="height: 150px" name="message"></textarea>
+                    <textarea class="form-control" placeholder="Leave a comment here" id="message" style="height: 150px"
+                        name="message" v-model="messageData.message" required></textarea>
                     <label for="message">Your message...</label>
                 </div>
                 <button type="submit" class="btn btn-primary mt-4">Submit</button>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,8 @@ const store = reactive({
     urlProjectsList: import.meta.env.VITE_API_PROJECTS_LIST_URL,
     projectDetailUrl: import.meta.env.VITE_API_PROJECT_DETAIL_URL,
     typesListUrl: import.meta.env.VITE_API_TYPES_LIST_URL,
-    technologiesListUrl: import.meta.env.VITE_API_TECHNOLOGIES_LIST_URL 
+    technologiesListUrl: import.meta.env.VITE_API_TECHNOLOGIES_LIST_URL,
+    messagesUrl: import.meta.env.VITE_API_MESSAGES_URL
 });
 
 export default store;
