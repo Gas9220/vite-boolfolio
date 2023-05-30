8edfb069eadcdfa2f2e6603b20e0ad2f62943d52
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 15:24:05 +0200

    TypesPage UI improvements

--- src/pages/TypesPage.vue
+++ src/pages/TypesPage.vue
@@ -37,7 +37,24 @@ export default {
     <main>
         <div class="container-fluid">
             <div class="pt-2" v-for="type in types">
-                <h2 class="text-white p-1 rounded">{{ type.name }}</h2>
+                <div class="accordion rounded" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header text-white">
+                            <button class="accordion-button custom-bg rounded text-white" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                {{ type.name }}
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show custom-bg rounded" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <span class="d-block">Slug: {{ type.slug }}</span>
+                                <span class="d-block">ID: {{ type.id }}</span>
+                                <span class="d-block">Created: {{ type.created_at }}</span>
+                                <span class="d-block">Updated: {{ type.updated_at }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="row gy-4 pt-4 pb-4">
                     <div v-for="project in type.projects" class="col-4">
                         <ProjectCard :project="project" />
@@ -49,8 +66,11 @@ export default {
 </template>
  
 <style scoped lang="scss">
-h2 {
-  background-color: #146C94;
-    
+.custom-bg {
+    background-color: #146C94;
+}
+
+.accordion-active-color {
+  background: blue
 }
 </style>
