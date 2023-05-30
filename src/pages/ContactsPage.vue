a50d2b1f947c3907c0b9d250c22302c70b337be4
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 17:14:53 +0200

    ContactsPageUI

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -8,7 +8,25 @@ export default {
 
 <template>
     <main>
-
+        <h1 class="text-center pt-2">Contact me</h1>
+        <div class="container-fluid justify-content-center d-flex">
+            <form class="w-50 pb-3 pt-3">
+                <div class="mb-3">
+                    <label for="user" class="form-label">Your name</label>
+                    <input type="text" class="form-control" id="user" aria-describedby="user" name="user">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
+                </div>
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="message"
+                        style="height: 150px" name="message"></textarea>
+                    <label for="message">Your message...</label>
+                </div>
+                <button type="submit" class="btn btn-primary mt-4">Submit</button>
+            </form>
+        </div>
     </main>
 </template>
  
